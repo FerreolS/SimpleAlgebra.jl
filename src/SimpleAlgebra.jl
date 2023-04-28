c38c7ec7342fc4f219db32bb5ba98984e91a2c51
ferreol <ferreol.soulez@free.fr>
2023-04-28 15:03:17 +0200

    Build gpu()

--- src/SimpleAlgebra.jl
+++ src/SimpleAlgebra.jl
@@ -1,6 +1,8 @@
 module SimpleAlgebra
-using ChainRulesCore
+import Flux: FluxCUDAAdaptor, gpu
 
+using ChainRulesCore
+using Flux
 struct SimpleAlgebraFailure <: Exception
     msg::String
 end
@@ -12,4 +14,14 @@ include("Domain.jl")
 include("Map.jl")
 include("LinOp/LinOp.jl")
 include("Cost/Cost.jl")
+
+
+function Flux.gpu(::Flux.FluxCUDAAdaptor,M::AbstractMap) 
+	n = nfields(M)
+	Mfields = (getfield(M,i) for i ∈ 1:n)
+	Mfields = map(gpu,Mfields)
+	eval(nameof(typeof(M)))(Mfields...)
+end
+#Adapt.adapt(Flux.FluxCUDAAdaptor(),L1)
+
 end # module SimpleAlgebra
