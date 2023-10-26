b5785342e88a4b5ab75ac9fa26d3c5a5f05d9f46
ferreol <ferreol.soulez@free.fr>
2023-10-26 15:18:22 +0200

    overload Base.in  when x is scalar

--- src/Domain.jl
+++ src/Domain.jl
@@ -8,7 +8,7 @@ Base.size(::Type{<:CoordinateSpace{S}}) where {S} = S
 numel(::Type{CoordinateSpace{S}}) where {S} = prod(S)
 
 Base.in(x::AbstractArray{T,N}, ::Type{S})  where {T,N,S<:CoordinateSpace} = size(S) == size(x)
-Base.in(x::T, ::Type{S})  where {T,S<:CoordinateSpace} = size(S) == (1,)
+Base.in(::T, ::Type{S})  where {T,S<:CoordinateSpace} = size(S) == (1,)
 
 Base.issubset(::Type{CoordinateSpace{S1}},::Type{CoordinateSpace{S2}}) where {S1,S2} = S1 ≤ S2
 Base.issubset(::Type{D},::Type{D}) where {D<:CoordinateSpace} = true
