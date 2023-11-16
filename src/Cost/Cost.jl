32b414c2fa5ac746a3d0a277b299d765e81f6f95
ferreol <ferreol.soulez@free.fr>
2023-11-16 11:57:39 +0100

    Add CostComposition

--- src/Cost/Cost.jl
+++ src/Cost/Cost.jl
@@ -1,6 +1,33 @@
 
 abstract type AbstractCost{I}  <: AbstractMap{I,Scalar{Real}}  end
 
-outputspace(A::AbstractCost) = Scalar{Real}()
+outputspace(::AbstractCost) = Scalar{Real}()
+
+
+																
+struct CostComposition{I,Left<:AbstractCost,Right<:AbstractMap} <:  AbstractCost{I}
+	left::Left
+	right::Right
+	function CostComposition(A::Dleft, B::Dright) where {Ileft, I2, O2,Dleft<:AbstractCost{Ileft},  Dright<:AbstractMap{I2,O2}} 
+		    return new{I2, Dleft,Dright}(A,B)
+	end
+end
+@functor CostComposition
+
+
+#= compose(A::AbstractCost{Ileft}, B::AbstractMap{Iright,Oright}) where {N,Tleft,
+																Tright<:Tleft,
+																Oright<:CoordinateSpace{Tright,N},
+																Ileft<:CoordinateSpace{Tleft,N},
+																Iright} = CostComposition(A, B) 
+ =#																
+compose(A::AbstractCost, B::AbstractMap) = CostComposition(A, B) 
+
+compose(::AbstractCost, AbstractMap) 	= throw(SimpleAlgebraFailure("Input size of first element $M does not match the output of the second element $N"))
+
+inputspace(A::CostComposition)  = inputspace(A.right)
+
+apply_(A::CostComposition, v) = apply(A.left,apply(A.right,v))
+
 
 include("./CostL2.jl")
