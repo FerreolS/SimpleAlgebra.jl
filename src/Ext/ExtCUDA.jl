7b5a98df7dcd3e7ad37b7e25b983a82dba8c3b28
ferreol <ferreol.soulez@free.fr>
2023-11-27 18:19:28 +0100

    Clean ExtCUDA

--- src/Ext/ExtCUDA.jl
+++ src/Ext/ExtCUDA.jl
@@ -1,27 +1,16 @@
 
-function Adapt.adapt_storage(::Type{CUDA.CuArray}, x::LinOpDFT) 
+function Adapt.adapt_storage(::Type{CUDA.CuArray}, x::LinOpDFT{I,O,F,B}) where  {I,O,F,B}
     dims = inputsize(x)
-	T = eltype(inputspace(x))
-    planning = planning = check_flags(FFTW.MEASURE)
-    timelimit = FFTW.NO_TIMELIMIT
-    # Compute the plans with suitable FFTW flags.  For maximum efficiency, the
-    # transforms are always applied in-place and thus cannot preserve their
-    # inputs.
+	T = eltype(I)
     
     if T<: fftwReal 
-        forward = plan_rfft(CUDA.CuArray{T}(undef, dims);
-                        flags = (planning | FFTW.PRESERVE_INPUT),
-                        timelimit = timelimit)
+        forward = plan_rfft(CUDA.CuArray{T}(undef, dims))
 
-        backward = plan_brfft(CUDA.CuArray{Complex{T}}(undef, forward.osz), dims[1];
-                          flags = (planning | FFTW.DESTROY_INPUT),
-                          timelimit = timelimit)
+        backward = plan_brfft(CUDA.CuArray{Complex{T}}(undef, forward.osz), dims[1];)
     else
         temp = CUDA.CuArray{T}(undef, dims)
-        forward = plan_fft!(temp; flags = (planning | FFTW.DESTROY_INPUT),
-                        timelimit = timelimit)
-        backward = plan_bfft!(temp; flags = (planning | FFTW.DESTROY_INPUT),
-                          timelimit = timelimit)
+        forward = plan_fft!(temp)
+        backward = plan_bfft!(temp)
     end
 
 
