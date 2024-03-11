e0d0d86df759f6dff0087f8c75135620d1d7ebf5
ferreol <ferreol.soulez@free.fr>
2024-03-11 16:13:44 +0100

    add scaledCostL2 (bugged?)

--- src/Cost/CostL2.jl
+++ src/Cost/CostL2.jl
@@ -48,3 +48,53 @@ function ChainRulesCore.rrule( ::typeof(apply_),A::CostL2, v)
     costL2_pullback(Δy) = (NoTangent(),NoTangent(), r .* Δy)
     return  sum(abs2,r)/2, costL2_pullback
 end
+
+
+
+struct scaledCostL2{I,D<:Union{AbstractArray,Number},P,N} <: AbstractCost{I}
+	inputspace::I
+	wdata::D
+	W::P    # precision matrix   
+	dims::N
+	scaledCostL2(inputspace::I, wdata::D,W::P,dims::N) where {I<:CoordinateSpace,D<:Union{AbstractArray,Number},P,N} =  new{I,D,P,N}(inputspace,wdata,W,dims)
+end
+
+@functor scaledCostL2
+ 
+
+function scaledCostL2(data::D; W=LinOpIdentity(size(data)),dims=Tuple( 1:N)) where {N,T,D<:AbstractArray{T,N}}  
+	if (W isa AbstractArray{T,N}) 
+		W = LinOpDiag(W)
+	end
+	(W isa AbstractLinOp) || throw(SimpleAlgebraFailure("W must a LinOp or an array"))
+
+	sz = size(data)
+	inputspace = CoordinateSpace(T,sz)
+	wdata = W*data
+	return scaledCostL2(inputspace, wdata,W,dims) 
+end
+
+function apply_(A::scaledCostL2, v) 
+	wv = A.W*v
+	α = sum(v.*A.wdata,dims=A.dims) ./ sum(wv.*v,dims=A.dims)
+	return sum(abs2,α .* wv .- A.wdata)/2
+end
+
+function apply_jacobian_(A::scaledCostL2, v, x)
+	wv = A.W*v
+	α = sum(v.*A.wdata,dims=A.dims) ./ sum(wv.*v,dims=A.dims)
+	return  α .* x .* (α .* wv .- A.wdata)
+end
+ 
+function getalpha(A::scaledCostL2, v) 
+	wv = A.W*v
+	return sum(v.*A.wdata,dims=A.dims) ./ sum(wv.*v,dims=A.dims)
+end
+
+function ChainRulesCore.rrule( ::typeof(apply_),A::scaledCostL2, v)
+	wv = A.W*v
+	α = sum(v.*A.wdata,dims=A.dims) ./ sum(wv.*v,dims=A.dims)
+	r = α .* wv .- A.wdata
+    costL2_pullback(Δy) = (NoTangent(),NoTangent(), α .* r .* Δy)
+    return  sum(abs2,r)/2, costL2_pullback
+end
